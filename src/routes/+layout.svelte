0870cffd23a26d92697b8e9d64bdaa29f17e10ac
tony.montemuro <tonyamontemuro@gmail.com>
2025-04-28 22:19:34 -0500

    begin setting up page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-	import '../app.css';
-
-	let { children } = $props();
+  import '../app.css';
+  let { children } = $props();
 </script>
 
-{@render children()}
+<header class="flex justify-between bg-blue-400 px-2 py-3">
+  <div>Tony Montemuro</div>
+  <div class="flex gap-3">
+    <a href="/about">Projects</a>
+    <a href="/blog">Blog</a>
+  </div>
+  <div class="flex gap-3">
+    <div>GitHub</div>
+    <div>LinkedIn</div>
+  </div>
+</header>
+
+<main class="p-2">
+  {@render children()}
+</main>
