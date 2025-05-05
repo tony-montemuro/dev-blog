adc3dbe2d24278fa9f7f5867de52c8b35cc2606a
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-04 22:26:32 -0500

    update technical skills

--- src/lib/types/technology.svelte
+++ src/lib/types/technology.svelte
@@ -1,19 +1,22 @@
 <script lang="ts" context="module">
   import AdobeCommerce from '$lib/components/svg/adobe-commerce.svelte';
+  import AWS from '$lib/components/svg/aws.svelte';
   import Git from '$lib/components/svg/git.svelte';
   import GNULinux from '$lib/components/svg/gnu-linux.svelte';
   import Go from '$lib/components/svg/go.svelte';
   import JavaScript from '$lib/components/svg/javascript.svelte';
   import JQuery from '$lib/components/svg/jquery.svelte';
+  import MaterialUi from '$lib/components/svg/material-ui.svelte';
   import MySQL from '$lib/components/svg/mysql.svelte';
   import Neovim from '$lib/components/svg/neovim.svelte';
+  import Npm from '$lib/components/svg/npm.svelte';
   import PHP from '$lib/components/svg/php.svelte';
   import Phpstorm from '$lib/components/svg/phpstorm.svelte';
   import PostgreSQL from '$lib/components/svg/postgresql.svelte';
   import React from '$lib/components/svg/react.svelte';
   import SvelteKit from '$lib/components/svg/sveltekit.svelte';
+  import Symfony from '$lib/components/svg/symfony.svelte';
   import TailwindCSS from '$lib/components/svg/tailwindcss.svelte';
-  import Tmux from '$lib/components/svg/tmux.svelte';
   import type { Component } from 'svelte';
 
   export interface Technology {
@@ -47,6 +50,11 @@
       name: 'SvelteKit',
       icon: SvelteKit,
       url: 'https://svelte.dev/'
+    },
+    {
+      name: 'Material UI',
+      icon: MaterialUi,
+      url: 'https://mui.com/material-ui/'
     }
   ];
 
@@ -65,6 +73,11 @@
       name: 'PHP',
       icon: PHP,
       url: 'https://www.php.net/'
+    },
+    {
+      name: 'Symfony',
+      icon: Symfony,
+      url: 'https://symfony.com/'
     },
     {
       name: 'MySQL',
@@ -95,14 +108,19 @@
       url: 'https://neovim.io/'
     },
     {
-      name: 'Tmux',
-      icon: Tmux,
-      url: 'https://github.com/tmux/tmux/wiki'
+      name: 'npm',
+      icon: Npm,
+      url: 'https://www.npmjs.com/'
     },
     {
       name: 'PHPStorm',
       icon: Phpstorm,
       url: 'https://www.jetbrains.com/phpstorm/'
+    },
+    {
+      name: 'Amazon Web Services',
+      icon: AWS,
+      url: 'https://aws.amazon.com/'
     }
   ];
 </script>
