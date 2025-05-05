a3be74df78d5e4ff9ca6ce0cb00ec9b604137799
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-04 19:55:50 -0500

    improve technology button style

--- src/lib/components/technologies.svelte
+++ src/lib/components/technologies.svelte
@@ -13,8 +13,12 @@
   <h3 class="text-xl">{name}</h3>
   <div class="flex gap-2">
     {#each technologies as technology}
-      <a href={technology.url} target="_blank">
-        <div class="flex w-fit flex-row items-center gap-1.5 rounded-full border p-3">
+      <a
+        href={technology.url}
+        target="_blank"
+        class="rounded-full border border-transparent bg-gray-400/30 transition-colors duration-300 ease-in-out hover:border-green-400 hover:bg-gray-400/60"
+      >
+        <div class="flex w-fit flex-row items-center gap-1.5 rounded-full p-3">
           <technology.icon />
           <span>{technology.name}</span>
         </div>
