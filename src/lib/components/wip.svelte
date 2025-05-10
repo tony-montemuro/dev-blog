61bdefd076a1f9eeaaa46ad047769aeca372eb26
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-10 14:09:58 -0500

    setup simple wip page

--- src/lib/components/wip.svelte
+++ src/lib/components/wip.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 </script>
 
-<div id="wip" class="h-app flex snap-start snap-always flex-col p-(--app-padding)">
-  <h1 class="text-7xl">WIP</h1>
+<div
+  id="wip"
+  class="h-app flex snap-start snap-always flex-col items-center justify-center bg-radial from-orange-400/50 to-orange-200/50 p-(--app-padding)"
+>
+  <h1 class="text-9xl">WIP...</h1>
 </div>
