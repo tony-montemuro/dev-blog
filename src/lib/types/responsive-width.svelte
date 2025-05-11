0cbaedd529b2a64ab9ddfbd8056155faf6bfae6f
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-10 23:15:40 -0500

    setup email feature on contact form

---
+++ src/lib/types/responsive-width.svelte
@@ -0,0 +1,6 @@
+<script lang="ts" context="module">
+  export interface ResponsiveWidth {
+    mobileWidth: number;
+    desktopWidth: number;
+  }
+</script>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,9 +1,60 @@
 <script lang="ts">
+  import emailjs from '@emailjs/browser';
   import Grid from '$lib/components/grid.svelte';
   import { onMount } from 'svelte';
+  import Spinner from '$lib/components/svg/spinner.svelte';
+
+  interface Message {
+    content: string | null;
+    color: 'green' | 'red';
+  }
+
+  let message: Message = $state({
+    content: null,
+    color: 'green'
+  });
+  let sending: boolean = $state(false);
+
+  const resetForm = (form: HTMLFormElement) => {
+    Array.from(form.querySelectorAll('input,textarea')).forEach((element) => {
+      (element as HTMLInputElement | HTMLTextAreaElement).value = '';
+    });
+  };
+
+  const sendEmail = (e: SubmitEvent) => {
+    e.preventDefault();
+
+    const target: HTMLFormElement = e.target as HTMLFormElement;
+    const SERVICE_ID = 'service_jyn9psh';
+    const TEMPLATE_ID = 'template_qeyiw0y';
+    const PUBLIC_KEY = 'w-CJKosHSbU9E5vp5';
+
+    sending = true;
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, target, {
+        publicKey: PUBLIC_KEY
+      })
+      .then(
+        () => {
+          message.content = 'Email sent! Thank you. 😀';
+          message.color = 'green';
+        },
+        (error) => {
+          message.content = `Email failed to send: ${error.text}`;
+          message.color = 'red';
+        }
+      )
+      .finally(() => {
+        resetForm(target);
+        sending = false;
+      });
+  };
+
+  let form: HTMLFormElement;
 
   onMount(() => {
     const inputs = document.querySelectorAll('.input');
+
     inputs.forEach((input, i) => {
       setTimeout(
         () => {
@@ -12,19 +63,19 @@
         },
         Math.pow(i, 1.25) * (i + 100)
       );
-      console.log(input);
     });
+
+    form.addEventListener('submit', sendEmail);
+    form.addEventListener('keypress', () => (message.content = null));
   });
 </script>
 
 <Grid />
 <div class="w-app lg:h-app flex items-center justify-center p-(--app-padding)">
-  <form class="flex w-200 flex-col gap-3">
+  <form bind:this={form} class="flex w-200 flex-col gap-3">
     <div class="animate-floatin flex flex-col gap-3">
       <h1 class="text-7xl">Contact</h1>
-      <span class="text-xl">
-        ✉️ Questions? Concerns? Please feel free to send me a message! I frequently check my email.
-      </span>
+      <span class="text-xl"> ✉️ Questions? Concerns? Please feel free to send me a message! </span>
     </div>
     <div class="input flex flex-col opacity-0">
       <label for="name" class="text-lg">Name</label>
@@ -66,15 +117,30 @@
         name="message"
         rows="7"
         class="rounded border border-gray-400 bg-gray-700/50 p-2 outline-none"
+        title="Not required, but highly recommended."
       ></textarea>
     </div>
 
-    <div>
+    <div class="flex items-center gap-3">
       <button
         type="submit"
-        class="cursor-pointer rounded border border-transparent bg-blue-500 p-3 text-xl transition-colors duration-300 ease-in-out hover:border-green-400"
-        >Send</button
+        class="{sending
+          ? 'cursor-auto border-green-400 bg-blue-400/80'
+          : 'cursor-pointer border-transparent bg-blue-500/80'} rounded border p-3 text-xl transition-colors duration-300 ease-in-out hover:border-green-400 hover:bg-blue-400/80"
       >
+        <div class="flex h-6.5 w-12 justify-center">
+          {#if sending}
+            <Spinner />
+          {:else}
+            Send
+          {/if}
+        </div>
+      </button>
+      {#if message.content}
+        <span class="text-lg {message.color === 'green' ? 'text-green-400' : 'text-red-400'}">
+          {message.content}
+        </span>
+      {/if}
     </div>
   </form>
 </div>
