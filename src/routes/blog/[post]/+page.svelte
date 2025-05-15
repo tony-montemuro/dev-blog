25a981044bff58c1dd3d05bc06997a536f681492
tony.montemuro <tonyamontemuro@gmail.com>
2025-05-14 22:11:31 -0500

    add background to single blog page

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -12,8 +12,11 @@
   let { meta, Content } = data;
 </script>
 
+<div class="top-nav h-app animate-fadein fixed right-0 bottom-0 left-0 -z-10">
+  <div class="h-full w-full bg-radial-[at_50%_0%] from-green-400/10 to-black/80"></div>
+</div>
 <article class="prose prose-invert lg:prose-xl animate-floatin mx-auto p-(--app-padding)">
-  <div class="mb-10">
+  <div class="mb-3">
     <h1>{meta.title}</h1>
     <div class="text-md flex flex-col">
       <span>
@@ -27,6 +30,9 @@
   </div>
   <div class="mb-10">
     <CategoryButtons categories={meta.categories} />
+  </div>
+  <div class="mb-10">
+    <hr />
   </div>
   <Content />
 </article>
